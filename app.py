7359bbc456dc1274358490e0b8fd9287674cfce0
mamadRb1 <mamadbosi1376@gmail.com>
2025-10-01 19:00:20 +0330

    fix: remove db dependency from sitemap

--- app.py
+++ app.py
@@ -1,52 +1,26 @@
-import os
 import datetime
-import sqlite3
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
-# صفحه اصلی سایت
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# گرفتن لیست صفحات فروشگاه از دیتابیس
-@app.route('/pages', methods=['GET'])
-def manage_pages():
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM pages')
-    rows = c.fetchall()
-    conn.close()
-    return {'pages': rows}
-
-# سایت‌مپ داینامیک
 @app.route('/sitemap.xml')
 def sitemap():
-    # صفحات ثابت
     static_pages = [
         {'loc': '/', 'priority': '1.0'},
         {'loc': '/shop', 'priority': '0.8'},
         {'loc': '/about', 'priority': '0.6'},
         {'loc': '/contact', 'priority': '0.6'}
     ]
-
-    # محصولات/صفحات از دیتابیس
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    c.execute('SELECT slug FROM pages')  # فرض: جدول pages ستون slug دارد
-    product_rows = c.fetchall()
-    conn.close()
-
-    products = [{'loc': f'/product/{slug}', 'priority': '0.7'} for (slug,) in product_rows]
-
-    pages = static_pages + products
     today = datetime.date.today().isoformat()
 
     xml = ['<?xml version="1.0" encoding="UTF-8"?>']
     xml.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
 
-    for page in pages:
+    for page in static_pages:
         xml.append('  <url>')
         xml.append(f'    <loc>https://cheshmashdecori.ir{page["loc"]}</loc>')
         xml.append(f'    <lastmod>{today}</lastmod>')
