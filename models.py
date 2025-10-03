0e27cb89dbeb423b28f3a9592b85cec2813f4131
mamadbosi1376 <mamadbosi1376@gmail.com>
2025-10-03 16:33:49 +0330

    Fix models.py fully and restore Product model

--- models.py
+++ models.py
@@ -5,7 +5,8 @@ db = SQLAlchemy()
 class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nu>
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
@@ -15,7 +16,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, null>
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
@@ -23,3 +24,11 @@ class User(db.Model):
 
 class Product(db.Model):
     __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(500))
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
