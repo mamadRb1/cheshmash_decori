28afc9ae9c810c2d6dd420b60e50ed7ea125ea4c
mamadbosi1376 <mamadbosi1376@gmail.com>
2025-10-03 16:03:20 +0330

    add db and bcrypt instances to config

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
-JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_here')
-SQLALCHEMY_DATABASE_URI = 'postgresql://cheshmash_products_db_user:vJggUZnjphGCrDUilf13cYAu0Ox2uSmz@dpg-d3ffaj2dbo4c73a7n2lg-a:5432/cheshmash_products_db'
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key_here')
+SQLALCHEMY_DATABASE_URI = 'postgresql://cheshmash_produciton_url_here'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
